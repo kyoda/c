5d4aedf5362048de9ca65d84c476fbdad0b88ba3
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-07-17 20:47:58 +0900

    modified:   first.c

--- first.c
+++ first.c
@@ -6,9 +6,16 @@
 
 #include <stdio.h>
 
+char *p, b;
+
 int main()
 {
-  printf("%p\n",&"abc");
+  if ((p = "abc")) {
+    printf("%p\n",p);
+  } else {
+    printf("%p\n",&"abc");
+  }
+
   return 0;
 }
 
