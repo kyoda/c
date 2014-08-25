7792cf4d226e79105ef3703fa3a745e4491784b1
kyoda <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-08-25 09:43:19 +0900

    new file:   forktest.c

---
+++ forktest.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+void sigchld_handler(int);
+
+int main() {
+
+    int sec;
+    signal(SIGCHLD, sigchld_handler);
+
+    for(;;) {
+
+        int pid;
+        scanf("%d", &sec);
+        pid = fork();
+
+        if(pid < 0) {
+            perror("fork");
+            exit(1);
+        }
+        else if(pid == 0) {
+            fprintf(stderr, "hello\n");
+            sleep(sec);
+            fprintf(stderr, "bye\n");
+            exit(0);
+        }
+
+    }
+
+}
+
+
+void sigchld_handler(int x) {
+
+    int chld;
+
+    chld = waitpid(-1, NULL, WNOHANG);
+
+    if(chld == -1) {
+        perror("wait");
+        exit(1);
+    }
+
+}
+
