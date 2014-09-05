71f66a2bae052448cc136308b215614b22c3686b
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-09-05 09:24:12 +0900

    modified:   bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -8,36 +8,47 @@ int main()
 {
 
   void bubbleSort(int p[], const int list[], const int listNum);
+
   int i;
 
   srand((unsigned)time(NULL));
-  const int listNum = rand() % 100;
+  const int listNum = 100 * 1000;
   int list[listNum];
   int p[listNum];
 
 
   for (i = 0; i<listNum; i++) {
-    list[i] = rand() % 1000;
+    list[i] = rand() % listNum;
   }
 
 
   // print array
+  /*
   printf("array: \n");
   for (i = 0; i<listNum; i++) {
     printf("%d ",list[i]);
   }
   printf("\n");
+  */
+  printf("finish create array \n");
+  
 
   // sort
+  clock_t start_t = clock();
   bubbleSort(p, list, listNum);
+  double stop_t = (double)(clock() - start_t) / CLOCKS_PER_SEC;
 
   // print result
+  /*
   printf("sorting array: \n");
   for (i = 0; i<listNum; i++) {
     printf("%d ",p[i]);
   }
   printf("\n");
+  */
 
+
+  printf("%f sec\n",stop_t);
 
   return 0;
 
