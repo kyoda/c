1fe2ef996a1f026e4b414bbdb78c687aef078547
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-09-26 09:18:13 +0900

    modified:   forktest.c

--- forktest.c
+++ forktest.c
@@ -28,15 +28,16 @@ int main()
 
         } else if (pid == 0) {
 
-            int list[] = {2, 7, 5, 1, 9, 6};
-            const int listNum = sizeof list;
-            int *p;
-            p = sort(list, listNum);
-
             fprintf(stderr, "hello\n");
             sleep(sec);
             fprintf(stderr, "bye\n");
             exit(0);
+
+        } else {
+
+          printf("%d \n",pid);
+          sleep(3);
+
 
         }
 
@@ -44,41 +45,6 @@ int main()
 
 }
 
-
-int *sort(const int list[], const int listNum)
-{
-
-  int p[listNum];
-  int i;
-
-  memcpy(p, list, listNum);
-
-  int index = 0, tmp = 0;
-
-  
-  for (i = 0; i<listNum - 1; i++) {
-    if (p[i] > p[i+1]) {
-       tmp = p[i];  
-       p[i] = p[i+1];
-       p[i+1] = tmp;
-       tmp = 0;
-    }
-    
-    if (i == listNum - 1) {
-      index++;
-      i = index; 
-    }
-
-  }
-
-  for (i = 0; i<listNum; i++) {
-    printf("%d ",p[i]);
-  }
-  printf("\n");
-
-  return 0;
-
-}
 
 
 void sigchld_handler(int x)
