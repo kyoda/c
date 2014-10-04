7429fe85f9672695c4cb20604d69e96c1b07f9b6
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-10-04 14:13:11 +0900

    modified:   test.s

--- test.s
+++ test.s
@@ -4,14 +4,14 @@ msg: .asciz "Hello, world\n"
 
 main:
 
-movl $1, %eax
-movl $0, %ebx
-int $0x80
-
 movl $4, %eax
 movl $1, %ebx
 movl $msg, %ecx
 movl $10, %edx
 int $0x80
 
+
+movl $1, %eax
+movl $0, %ebx
+int $0x80
 
