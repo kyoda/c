1999c61696a93ff21349c59df8ee99dda020afcd
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-11-13 09:19:21 +0900

    modified:   unsigned.c

--- unsigned.c
+++ unsigned.c
@@ -6,9 +6,14 @@ int main()
   i = -1;
   
   printf("sizeof = %d\n",(short unsigned int)sizeof(i));
-  printf("i = %d\n",i);
+  printf("i = %d\n", i);
   i = i + 10;
-  printf("i = %d\n",i);
+  printf("i = %d\n", i);
+
+
+  unsigned long long max;
+  max = -1;
+  printf("%llu\n", max);
 
   return 0;
 }
