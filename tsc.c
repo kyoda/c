707dec461958b8ecabec3476afbdc94a5f75346f
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-11-19 09:35:07 +0900

    modified:   tsc.c

--- tsc.c
+++ tsc.c
@@ -11,9 +11,12 @@ int main()
   tsc = ((unsigned long long)lo | ((unsigned long long)hi << 32));
   printf("%llu\n", tsc);
 
-  unsigned long long day;
-  day = tsc / 2333338000 / (60 * 60 * 24);
+  unsigned long long hz, day, sec, hour;
+  hz = 2333338000;
+  day = tsc / hz / (60 * 60 * 24);
+  sec = (tsc / hz) - (60 * 60 * 24);
   printf("%llu\n", day);
+  printf("%llu\n", hour);
 
   return 0;
 
