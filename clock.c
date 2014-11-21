5a59353e1ac23cdb7ee0b0d43132cc96af930051
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-11-21 09:12:50 +0900

    modified:   clock.c

--- clock.c
+++ clock.c
@@ -1,10 +1,12 @@
 #include <time.h>
 #include <stdio.h>
+#include <unistd.h>
 
 
 int main ()
 {
 
+  /*
   int rc;
   struct timespec res;
 
@@ -17,6 +19,26 @@ int main ()
   if (!rc) {
     printf("CLOCK_REALTIME: %ldns\n", res.tv_nsec);
   }
+  */
+
+
+  
+  unsigned long lo, hi;
+  unsigned long long tsc_begin, tsc_end;
+  double clock;
+
+  asm volatile("rdtsc": "=a" (lo), "=d" (hi));
+  tsc_begin = ((unsigned long long)lo | ((unsigned long long)hi << 32));
+
+  sleep(1);
+
+  asm volatile("rdtsc": "=a" (lo), "=d" (hi));
+  tsc_end = ((unsigned long long)lo | ((unsigned long long)hi << 32));
+
+  clock = (double)((tsc_end - tsc_begin) / 1000000);
+
+  printf("%.2f MHz\n", clock);
 
   return 0;
+
 }
