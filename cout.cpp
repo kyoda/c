1ef79a0d9c9b0bd1cdb13da0e54d0e26aca0482a
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-11-26 10:00:22 +0900

    new file:   cout.cpp

---
+++ cout.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+
+template<typename T_n>
+T_n Multiply(T_n A, T_n B)
+{
+  return A * B;
+}
+
+int main()
+{
+  void *i;
+  i = std::cout << "aiueo\n";
+  printf("%p",i);
+
+  int a;
+  a = Multiply<int>(3, 5);
+  printf("%d\n",a);
+
+  return 0;
+
+}
+
