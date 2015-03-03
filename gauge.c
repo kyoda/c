c5af360bdf06eef4a01e2951eb04407af975f355
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-03-03 21:54:50 +0900

    modified:   gauge.c

--- gauge.c
+++ gauge.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 int main(void)
 {
 
+  setbuf(stdout, NULL);
+
+  char str[101];
+  int l = sizeof str;
+  for (int i = 0; i < l; i++) {
+    str[i] = ' ';
+  } 
+
   for (int i = 0; i < 100; i++) {
-    printf("count: %3d %%\r", i);
-    fflush(stdout);
+    str[i] = '#';
+    printf("[%s]: %3d %%\r", str, i);
     sleep(1);
   }
 
