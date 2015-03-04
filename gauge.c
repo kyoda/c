0606431bf559ab39ebd790d16493e4f81a21b6b6
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-03-05 08:50:09 +0900

    modified:   gauge.c

--- gauge.c
+++ gauge.c
@@ -7,18 +7,23 @@ int main(void)
 
   setbuf(stdout, NULL);
 
-  char str[101];
+  char str[100];
   int l = sizeof str;
   for (int i = 0; i < l; i++) {
     str[i] = ' ';
   } 
-  str[100] = '\0';
 
-  for (int i = 0; i < 101; i++) {
+  for (int i = 0; i < l + 1; i++) {
     printf("[%s]: %3d %% ", str, i);
-    str[i] = '#';
     sleep(1);
-    printf("%c", i == 100?'\n':'\r');
+    if (i == 100) {
+      printf("\n");
+      break;
+    }
+
+    str[i] = '#';
+    printf("\r");
+
   }
 
   return 0;
