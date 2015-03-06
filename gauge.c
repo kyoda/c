224d02c8980ed82257e1b4e6c5be57331604bceb
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-03-06 09:16:19 +0900

    modified:   gauge.c

--- gauge.c
+++ gauge.c
@@ -2,20 +2,24 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef unsigned int useconds_t;
+
 int main(void)
 {
+  int i;
 
   setbuf(stdout, NULL);
 
   char str[100];
   int l = sizeof str;
-  for (int i = 0; i < l; i++) {
+  for (i = 0; i < l; i++) {
     str[i] = ' ';
   } 
 
-  for (int i = 0; i < l + 1; i++) {
+  useconds_t wait_time = 100000;
+  for (i = 0; i < l + 1; i++) {
     printf("[%s]: %3d %% ", str, i);
-    sleep(1);
+    usleep(wait_time);
     if (i == 100) {
       printf("\n");
       break;
