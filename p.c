e947a2ad2a5df893d3c522cdc761fb01a4743f71
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-03-20 09:04:51 +0900

    modified:   p.c

--- p.c
+++ p.c
@@ -14,10 +14,14 @@ int main(void)
   }
 
   char *str = "aiueoaiueo";
-  printf("String = %s, Length = %lu\n", str, sizeof(str));
+  printf("String = %s, Length = %lu\n", str, sizeof(str) / sizeof(char));
 
   char str2[11];
   printf("String = %s, Length = %lu\n", str2, sizeof(str2) / sizeof(char));
+
+  char *q;
+  q = str2;
+  printf("String = %s, Length = %lu\n", q, sizeof(q) / sizeof(char));
 
 
   return 0;
