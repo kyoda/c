6b1aa040bc0642e868fb17719e397477a5f8073b
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-04-16 20:23:55 +0900

    modified:   inode.c

--- inode.c
+++ inode.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv)
     printf("%d\n", (int)stat_buf.st_gid);
     printf("%d\n", (int)stat_buf.st_rdev);
     printf("%d\n", (int)stat_buf.st_size);
+    printf("%d\n", (int)stat_buf.st_atime);
   }
 
   return 0;
