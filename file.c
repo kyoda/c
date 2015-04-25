2d8241bbc823dc62ac0ddc5b66da3125a8ea0230
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-04-25 22:43:34 +0900

    modified:   file.c

--- file.c
+++ file.c
@@ -8,10 +8,13 @@ int main()
 {
 
   int fd = open("README.md", O_RDONLY);
-  int fd2 = open("README.md", O_RDONLY);
+  int fd2 = open("READMEEE.md", O_RDONLY);
   printf("%d\n", fd);
   printf("%d\n", fd2);
   //int r = flock(fd, LOCK_SH);
+
+  FILE *fp;
+  fp = fopen("README.md", "r");
 
   return 0;
 }
