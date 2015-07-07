fbb9e803b5e2bae5ef9a0f079574d06e1baa5b83
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-07-07 09:22:04 +0900

    modified:   ifvsswitch.c

--- ifvsswitch.c
+++ ifvsswitch.c
@@ -2,58 +2,100 @@
 #include <stdlib.h>
 #include <time.h>
 
-//#define MAX 2150000000
-#define MAX 2100000000
+#define MAX 100000000
 
 int ifcal()
 {
 
-  int a = 0, b = 0;
+  int a = 0;
+  double b = 0;
 
   int i = 0;
   clock_t start_t = clock();
 
   for (; i < MAX; i++) {
+    a = rand();
     if (a == 0) {
-      b += a;     
+      b += (double)a;     
     } else if (a == 1) {
-      b += a;     
+      b += (double)a;     
+    } else if (a == 2) {
+      b += (double)a;     
+    } else if (a == 3) {
+      b += (double)a;     
+    } else if (a == 4) {
+      b += (double)a;     
+    } else if (a == 5) {
+      b += (double)a;     
+    } else if (a == 6) {
+      b += (double)a;     
+    } else if (a == 7) {
+      b += (double)a;     
+    } else if (a == 8) {
+      b += (double)a;     
+    } else if (a == 9) {
+      b += (double)a;     
     }
   }
 
-  double stop_t = (double)(clock() - start_t);
+  double stop_t = (double)(clock() - start_t) / CLOCKS_PER_SEC;
 
-  printf("b = %d\n", b);
+  printf("b = %d\n", (int)b);
   printf("ifcal: %f sec\n", stop_t);
 
-  return b;
+  return (int)b;
 
 }
 
 int switchcal()
 {
 
-  int a = 0, b = 0;
+  int a = 0;
+  double b = 0;
 
   int i = 0;
 
   clock_t start_t = clock();
 
   for (; i < MAX; i++) {
+    a = rand();
     switch (a) {
     case 0:
-      b += a;
+      b += (double)a;
       break;
     case 1:
-      b += a;
+      b += (double)a;
+      break;
+    case 2:
+      b += (double)a;
+      break;
+    case 3:
+      b += (double)a;
+      break;
+    case 4:
+      b += (double)a;
+      break;
+    case 5:
+      b += (double)a;
+      break;
+    case 6:
+      b += (double)a;
+      break;
+    case 7:
+      b += (double)a;
+      break;
+    case 8:
+      b += (double)a;
+      break;
+    case 9:
+      b += (double)a;
       break;
     }
   }
 
-  double stop_t = (double)(clock() - start_t);
-  //double stop_t = (double)(clock() - start_t) / CLOCKS_PER_SEC;
+  double stop_t = (double)(clock() - start_t) / CLOCKS_PER_SEC;
 
-  printf("b = %d\n", b);
+  printf("b = %d\n", (int)b);
   printf("switchcal: %f sec\n", stop_t);
 
 
@@ -70,6 +112,7 @@ int main()
   ifcal();
   switchcal();
 
+  printf("%d\n", rand());
 
   return 0;
 
