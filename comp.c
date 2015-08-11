44da151267255ea40f6f2461bbc825c04bbda701
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-08-11 20:49:21 +0900

    modified:   comp.c

--- comp.c
+++ comp.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/file.h>
 
 int main()
@@ -6,6 +7,9 @@ int main()
 
   
   int fd = open("README.md", O_RDONLY);
+  int a = 0;
+  a = read(fd, 0, 32);
+  printf("%d\n", a);
   close(fd);
 
   return 0;
